732f00707deb271c38b2f772bb8eedca512824f7
rjaeka456 <hoon4569@gmail.com>
2017-09-11 18:24:59 +0900

    170911 보안편지쓰기 버튼 추가
    매니페스트 파일 수정 (공백 삭제)
    
    버튼 추가를 위한 코드 추가

--- contentscript.js
+++ contentscript.js
@@ -1,1 +1,19 @@
-alert(2);
+window.addEventListener ("load", myMain, false);
+
+function myMain (evt) {
+   
+    btnWriteMail = document.querySelector('div.z0');
+   
+    btnWriteMail.insertAdjacentHTML("afterend",'<div class="T-I J-J5-Ji T-I-KE L3" role="button" tabindex="1" style="user-select: none;" id="btnSecuMail">보안편지쓰기</div>');
+   
+    btnSecuMail = document.querySelector('#btnSecuMail');
+    
+    btnSecuMail.addEventListener("click", function(a){
+        alert(1);
+    })
+}
+
+
+
+
+
